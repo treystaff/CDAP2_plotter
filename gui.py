67b738552d66eb6d22fcac40e2c36a14f81df7e9
Trey Stafford <treystaff@gmail.com>
2014-11-06 17:56:36 -0600

    Can now reliably change a line's color. Added optional legend and ability to add custom legend labels.

--- gui.py
+++ gui.py
@@ -13,6 +13,7 @@ class scanData:
     self.selectedScans = []
     self.scanIds = []
     self.db = db
+    self.app = app
 
   def add(self,scaninfo):
     #Currently, scaninfo is a dictionary
@@ -36,12 +37,12 @@ class scanData:
 
     band,ref = zip(*data)
 
-    #Add the matplotlib 2D line obj
+    #Add the matplotlib 2D line obj & set default label (rep name + (x,y)
     scaninfo['line'], = plt.plot(band,ref)
+    scaninfo['line'].set_label(scaninfo['rep']+': ('+scaninfo['x']+','+scaninfo['y']+')')
 
     #Add a dict of line properties (properties filled in by user)
-    scaninfo['properties'] = {'color':None,'thickness':None,'style':None,
-      'label':None}
+    scaninfo['properties'] = {'thickness':None,'style':None}
     self.selectedScans.append(scaninfo)
 
   def remove(self,idx):
@@ -55,12 +56,29 @@ class scanData:
     if self.currentSelection is not None:
       return self.selectedScans[self.currentSelection]
 
+  def get_line_color(self):
+    #Becuase matplotlib's default colors are represented by letters,
+    # make sure everything is HEX for consistency
+    colorToHex = {'c': '#00bfbf', 'b': '#0000ff',
+      'w': '#ffffff', 'g': '#008000', 'y': '#bfbf00',
+      'k': '#000000', 'r': '#ff0000', 'm': '#bf00bf'}
+    color = self.get_cur_scan()['line'].get_color()
+    if color in colorToHex.keys():
+      return colorToHex[color]
+    else:
+      return color
+
+  def set_label(self,text):
+    if text:
+      self.get_cur_scan()['line'].set_label(text)
+
 class app:
   def __init__(self,master):
     self.master = master
     self.master.title("CDAP2_Plotter")
     self.master.columnconfigure(0,weight=1)
     self.master.rowconfigure(0,weight=1)
+    self.style = ttk.Style()
 
     #Create the tab structure.
     self.create_tabs()
@@ -107,6 +125,8 @@ class app:
     self.create_metadata_box()
     self.create_t3_frames()
     self.create_line_color_button()
+    self.create_line_label_entry()
+    self.create_legend_checkButton()
 
   def create_project_dropdown(self):
     #Create a label for the dropdown
@@ -144,13 +164,41 @@ class app:
     self.removeScanB.grid(column=1,row=2,sticky=(E))
 
   def create_line_color_button(self):
+    self.style.configure('lineColorB.TButton')
     self.lineColorB = ttk.Button(self.linePropertiesFrm,text='Choose Color',
-      command=self.choose_line_color)
-    self.lineColorB.grid(column=1,row=1,sticky=(E))
+      command=self.choose_line_color,style='lineColorB.TButton')
+    self.lineColorB.grid(column=1,row=2,sticky=(E))
 
     #ALSO A LABEL
     self.colorLabel = ttk.Label(self.linePropertiesFrm,text='Line Color:')
-    self.colorLabel.grid(column=0,row=1,sticky=(W))
+    self.colorLabel.grid(column=0,row=2,sticky=(W))
+
+  def create_line_label_entry(self):
+    self.lineLabelVar = StringVar()
+    self.lineLabelTB = ttk.Entry(self.linePropertiesFrm,textvariable=self.lineLabelVar)
+    self.lineLabelTB.bind("<Return>",self.set_label)
+    self.lineLabelTB.grid(column=1,row=1,sticky=(W))
+
+    #Label the label!
+    self.lineLabelLabel = ttk.Label(self.linePropertiesFrm,text='Legend Label')
+    self.lineLabelLabel.grid(column=0,row=1)
+
+  def set_label(self,event):
+    self.scanData.set_label(self.lineLabelVar.get())
+    if self.legendCBVar.get():
+      plt.legend().set_visible(True)
+
+  def create_legend_checkButton(self):
+    self.legendCBVar = IntVar()
+    self.legendCB = ttk.Checkbutton(self.graphPropertiesFrm,variable=self.legendCBVar,
+      command=self.activate_legend,text='Show Legend')
+    self.legendCB.grid(column=0,row=6)
+
+  def activate_legend(self):
+    if self.legendCBVar.get():
+      plt.legend().set_visible(True)
+    else:
+      plt.legend().set_visible(False)
 
   def create_selected_scans_listboxes(self):
     #Tab2 listbox
@@ -169,7 +217,14 @@ class app:
     else:
       self.scanData.currentSelection = int(self.selectedListboxT3.curselection()[0])
       self.selectedListboxT2.activate(self.scanData.currentSelection)
+    #populate metadata
     self.populate_metadata()
+    #Change color of color button to reflect current scan's line color.
+    self.style.configure('lineColorB.TButton',background = self.scanData.get_line_color())
+
+    #Change legend label text
+    self.lineLabelTB.delete(0,END)
+    self.lineLabelTB.insert(0,self.scanData.get_cur_scan()['line'].get_label())
 
   def create_t3_frames(self):
     self.linePropertiesFrm = ttk.Frame(self.t3)
@@ -205,10 +260,17 @@ class app:
 
   def choose_line_color(self):
     selectedColor = colorchooser.askcolor()
-    if selectedColor:
+    if selectedColor[-1]:
       plt.setp(self.scanData.get_cur_scan()['line'],
         color=selectedColor[-1])
       self.scanData.get_cur_scan()['properties']['color'] = selectedColor[-1]
+
+      #Change the button's color to reflect the change
+      self.style.configure('lineColorB.TButton',background = selectedColor[-1])
+
+      #Update the legend if it is being shown.
+      if self.legendCBVar.get():
+        plt.legend().set_visible(True)
 
   def populate_scans(self,event):
     #FUNCTION TO POPULATE THE SCANS TREE.
@@ -222,8 +284,8 @@ class app:
 
     #Add every scan to the tree.
     if scans:
-      uniqueReps = set([scan[1] for scan in scans])
       #CODE TO NEST SCANS UNDER REPS THAT EXPAND.
+      uniqueReps = set([scan[1] for scan in scans])
       for rep in uniqueReps:
         self.tree.insert('','end',rep,text=rep)
       for scan in scans:
